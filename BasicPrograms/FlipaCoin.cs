c64fc8ef1a5dfc69b296d5c174a4d70766bed2dc
MOHAMEDAfrath <afrathismath@gmail.com>
2021-06-29 16:40:54 +0530

    [Afrath] Updated the file with Flip Coin Program

---
+++ BasicPrograms/FlipaCoin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    class FlipaCoin
+    {
+        public static void HeadOrTail()
+        {
+            int head = 0;
+            int tail = 0;
+            Random random = new Random();
+            int noOfFlips = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < noOfFlips; i++)
+            {
+                double flip = (double)random.NextDouble();
+                if (flip > 0.5)
+                {
+                    head++;
+
+                }
+                else
+                {
+                    tail++;
+                }
+            }
+                Console.WriteLine("No Of Heads : " + head);
+                Console.WriteLine("No Of Tails : " + tail);
+                double headPercent= (double)head/noOfFlips *100;
+                double tailPercent = (double)tail/noOfFlips *100 ;
+                Console.WriteLine("Head Percentage : "+headPercent);
+                Console.WriteLine("Tail Percentage : "+tailPercent);
+            
+        }
+
+    }
+
+}
--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -7,6 +7,7 @@ namespace BasicPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Programming!");
+            FlipaCoin.HeadOrTail();
         }
     }
 }
