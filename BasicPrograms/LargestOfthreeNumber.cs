ec2d72d07e357ff0daa0925ece02bdd248f1a7da
MOHAMEDAfrath <afrathismath@gmail.com>
2021-06-29 20:07:42 +0530

    [Afrath] Updated the file with largest of three numbers program

---
+++ BasicPrograms/LargestOfthreeNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    /// <summary>
+    /// Find the largest number among the three numbers.
+    /// </summary>
+    class LargestOfthreeNumber
+    {
+        public static void LargestNumber()
+        {
+            Console.WriteLine("Enter the three Numbers:");
+            int[] numbers = new int[3];
+            for(int i = 0; i < 3; i++)
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                numbers[i] = a;
+            }
+            int result = (numbers[0] > numbers[1]) ? (numbers[0] > numbers[2] ? numbers[0] : numbers[2]) : (numbers[1] > numbers[2] ? numbers[1] : numbers[2]);
+            Console.WriteLine("The largest number among [ " +numbers[0]+ " " +numbers[1]+ " "+numbers[2]+ " ] is "+result);
+        }
+    }
+}
--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -4,18 +4,21 @@ namespace BasicPrograms
 {
     class Program
     {
+        
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Programming!");
             //FlipaCoin.HeadOrTail();
             //LeapYear.CheckLeapYear();
             //PowerOf2.Powerof2Table();
-            // HarmonicNumber.HarmonicSeq();
+            //HarmonicNumber.HarmonicSeq();
             //FactorProgram.FactorMethod();
             //QuotientandRemainder.Division();
             //SwapNumbers.GetInput();
             //EvenorOdd.CheckEvenorOdd();
-            VowelorConsonants.CheckVowelOrConsonants();
+            //VowelorConsonants.CheckVowelOrConsonants();
+            LargestOfthreeNumber.LargestNumber();
 
         }
     }
